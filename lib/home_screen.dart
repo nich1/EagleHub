b5243caeaddb46de593f085dc3be9ccc65f7e43d
nich1 <91976504+nich1@users.noreply.github.com>
2024-07-23 15:53:36 -0500

    Fixed URL bug on android

--- lib/home_screen.dart
+++ lib/home_screen.dart
@@ -56,7 +56,7 @@ class _HomeScreenState extends State<HomeScreen> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text('UNT Dining Services'),
+        title: Text('Eagle Hub'),
         actions: [],
       ),
       body: IndexedStack(
